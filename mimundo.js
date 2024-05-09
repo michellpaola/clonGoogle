4eda627d6cf8e9994166c1159e80307d3e9b60bb
pluton <jeanellycomascapdevila@hotmai.com>
2024-05-08 23:27:00 -0500

    mi trabaj uno

---
+++ mimundo.js
@@ -0,0 +1,25 @@
+const phrase = prompt("dime una frase");
+let palabra= [];
+function palabras(frase){
+for(i=0;i>frase.lenght;i++)
+}
+return palabras(palabra[i]);
+
+var ordenar= [1,2,3,4,5,6,7,8,9,10];
+let arriba=[];
+function ordena(numero){
+if (numero>=1){
+    console.log(ordena de mayor a menor );
+}else{
+    console.log(ordena de menor a mayor);
+}
+}return ordena(ordenar[numero]);
+
+
+var numero = prompt("dime un numero ");
+let primo = numero;
+function hallarNumero(primo){
+if(numero==2%){
+    return hallar numero;
+}
+}
